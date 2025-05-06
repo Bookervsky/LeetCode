a8df405078b547ed9877aa21a5e7716b578148c4
LYU BUKE <54054884+Bookervsky@users.noreply.github.com>
2025-05-06 16:04:25 +0800

    0148-sort-list ,05-06-2025

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = findMid(head);
+        ListNode right = sortList(mid.next);
+        mid.next = null;
+        ListNode left = sortList(head);
+        left = mergesortedList(left, right);
+        return left;
+    }
+
+    public ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode mergesortedList(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        if (left == null) {
+            cur.next = right;
+        }
+        if (right == null) {
+            cur.next = left;
+        }
+        return dummy.next;
+    }
+}
