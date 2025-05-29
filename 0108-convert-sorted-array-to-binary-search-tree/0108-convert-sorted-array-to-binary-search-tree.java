dfae8c5da53fa46faca18d90d23b8192a27f820e
LYU BUKE <54054884+Bookervsky@users.noreply.github.com>
2025-05-29 18:01:44 +0800

    0108-convert-sorted-array-to-binary-search-tree

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root = help(nums, 0, nums.length);
+        return root;
+    }
+    public TreeNode help(int[] nums, int left, int right){
+        if (left == right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = help(nums, left, mid);
+        root.right = help(nums, mid+1, right);
+        return root;
+    }
+}
