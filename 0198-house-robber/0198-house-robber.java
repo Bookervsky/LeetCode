3246ef04df7a6b7af36ffcb8c3c84dd2e7e59ce0
LYU BUKE <54054884+Bookervsky@users.noreply.github.com>
2025-05-30 17:58:37 +0800

    0198-house-robber

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        sum[1] = Math.max(nums[0],nums[1]);
+        for (int i= 2; i<nums.length; i++){
+            sum[i] = Math.max(sum[i-1], sum[i-2]+nums[i]);
+        }
+        return sum[nums.length-1];
+    }
+}
