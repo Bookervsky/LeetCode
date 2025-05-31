6e2edc392f8edfa08227205bc08210c170abbf81
LYU BUKE <54054884+Bookervsky@users.noreply.github.com>
2025-05-31 11:40:00 +0800

    0001-two-sum

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> cache = new HashMap<>();
+        for (int i=0; i<nums.length; i++){
+            if (cache.containsKey(target - nums[i])) return new int[]{cache.get(target - nums[i]), i};
+            cache.put(nums[i], i);
+        }
+        return null;
+    }
+}
